1746adf7883f613e21b4c8182280c3fe5e1b5234
ERIC10000 <erickolattiz@gmail.com>
2022-11-08 13:32:07 +0300

    First Commit

--- main.py
+++ main.py
@@ -40,4 +40,58 @@ def login():
         return response
 
 
+
+@app.route('/change_password', methods=['POST'])
+def change_password():
+            json = request.json
+            driver_id = json['driver_id']
+            current_password = json['current_password']
+            new_password =json['new_password']
+            confirm_password = json['confirm_password']
+
+            sql = "select * from drivers where driver_id = %s"
+            cursor = connection.cursor()
+            cursor.execute(sql, (driver_id))
+            # get row containing the current password from DB
+            row = cursor.fetchone()
+            hashed_password = row[6]
+            status = password_verify(current_password, hashed_password)
+            if status:
+                print("Current is okay")
+                response = passwordValidity(new_password)
+                print("tttttttttt", response)
+                if response == True:
+                    print("New is okay")
+                    if new_password != confirm_password:
+                        response = jsonify({'msg': 'Password Do Not Match'})
+                        response.status_code = 401
+                        return response
+                    else:
+                        print("Confirm is okay")
+                        sql = "update users set password = %s where user_id = %s"
+                        cursor = connection.cursor()
+                        try:
+                            cursor.execute(sql, (password_hash(new_password), driver_id))
+                            connection.commit()
+                            response = jsonify({'msg': 'password Changed'})
+                            response.status_code = 200
+                            return response
+                        except:
+                            connection.rollback()
+                            response = jsonify({'msg': 'Password was Not Changed'})
+                            response.status_code = 401
+                            return response
+                else:
+                    response = jsonify({'msg': response})
+                    response.status_code = 401
+                    return response
+
+            else:
+                response = jsonify({'msg': 'Current Password is Wrong'})
+                response.status_code = 401
+                return response
+
+
+
+
 app.run(debug=True)
